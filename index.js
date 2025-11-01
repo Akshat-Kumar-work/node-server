2b4a9b065e3f40188a148ece9b819f1314114ef0
Akshat <iamakku0.0.0.1@gmail.com>
2025-11-01 21:55:05 +0530

    simple node server

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000; // fallback if not set
+
+const test_var = process.env.test;
+
+console.log(test_var);
+
+app.get('/', (req, res) => {
+  res.send(`Server running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
